0894617f92882e64e6ad80a04dfadd8998ebbd57
Humberto Cristian Paez <crishpaez1993@gmail.com>
2022-03-03 12:13:13 -0300

    Add project files.

---
+++ StarWars/Controllers/Players.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StarWars.Controllers
+{
+    public class Players : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
